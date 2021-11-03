21ea8eb9bdeed623ce782a4bf82ad0edc8c0dd7c
Wilson Júnior <wilsonpjunior@gmail.com>
2021-11-03 10:07:11 -0300

    Add index page

--- server.go
+++ server.go
@@ -13,8 +13,13 @@ type server struct {
 
 func (s *server) start() error {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", s.index)
 	mux.HandleFunc("/metrics", promhttp.Handler().ServeHTTP)
 	addr := "0.0.0.0:" + s.config.port
 	log.Printf("Listening on %v", addr)
 	return http.ListenAndServe(addr, mux)
+}
+
+func (s *server) index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("tsuru/tsuru-team-metrics running"))
 }
